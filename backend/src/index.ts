73963437e99fa283c5b295854334aadf6088122d
Aditya Karki <adityakarki201@gmail.com>
2025-09-01 15:20:39 +0530

    Add: Cookie Parser

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -2,25 +2,29 @@ import express, { Request, Response } from "express";
 import dotenv from "dotenv";
 import appRouterV1 from "./routes/v1";
 import db, { userTableCmd } from "./db/db.schema";
+import cookies from "cookie-parser";
 
-//to use env variables
+// to use env variables
 dotenv.config();
 
-//initializing express application
+// initializing express application
 const app = express();
 
 const port = process.env.PORT || 3000;
 
-//middleware to use parse the body
+// middleware to use parse the body
 app.use(express.json());
 
-//execute create table command
+// middleware to parse the cookies
+app.use(cookies());
+
+// execute create table command
 db.exec(userTableCmd);
 
-//routing every request to version 1 routes
+// routing every request to version 1 routes
 app.use(appRouterV1);
 
-//server listening port
+// server listening port
 app.listen(port, () => {
   console.log(`app is listening at port`, port);
 });
