d722da779b5e90f508640faa12d990d2875fecd0
Aditya Karki <adityakarki201@gmail.com>
2025-09-02 23:04:38 +0530

    Add: API call in Register

--- frontend/src/components/Register.tsx
+++ frontend/src/components/Register.tsx
@@ -1,4 +1,3 @@
-import React from "react";
 import { registerSchema } from "../Schema/schema.login";
 import { BotMessageSquare } from "lucide-react";
 import { useForm, type SubmitHandler } from "react-hook-form";
@@ -21,6 +20,26 @@ const Register = () => {
   });
   const onSubmit: SubmitHandler<FormValues> = (data) => {
     console.log(data);
+    //make api call
+    fetch("http://localhost:3001/api/v1/user/register", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(data),
+    })
+      .then((res) => {
+        if (!res.ok) {
+          throw new Error("Network response was not ok");
+        }
+        return res.json();
+      })
+      .then((data) => {
+        console.log(data);
+      })
+      .catch((error) => {
+        console.error("There was a problem with the fetch operation:", error);
+      });
   };
   return (
     <div className="bg-gray-800 text-gray-100 rounded-md border border-white/20 p-3 w-full max-w-sm shadow-xl">
@@ -30,37 +49,48 @@ const Register = () => {
       <BotMessageSquare className="w-16 h-16 mx-auto mb-4 text-blue-400" />
       <form onSubmit={handleSubmit(onSubmit)}>
         <div className="flex flex-col gap-5">
-          <input
-            type="text"
-            {...register("email")}
-            placeholder="Email Address"
-            className={`bg-gray-700 text-gray-100 rounded-md border  p-2 outline-none ${
-              errors.email ? "border-red-500" : "border-white/20"
-            }`}
-          />
-          <span className="text-red-500 text-sm">{errors.email?.message}</span>
-          <input
-            type="password"
-            {...register("password")}
-            placeholder="Password"
-            className={`bg-gray-700 text-gray-100 rounded-md border p-2 outline-none ${
-              errors.password ? "border-red-500" : "border-white/20"
-            }`}
-          />
-          <span className="text-red-500 text-sm ">
-            {errors.password?.message}
-          </span>
-          <input
-            type="password"
-            {...register("confirmPassword")}
-            placeholder="Confirm Password"
-            className={`bg-gray-700 text-gray-100 rounded-md border p-2 outline-none ${
-              errors.confirmPassword ? "border-red-500" : "border-white/20"
-            }`}
-          />
-          <span className="text-red-500 text-sm ">
-            {errors.confirmPassword?.message}
-          </span>
+          <div className="flex flex-col gap-2">
+            <label htmlFor="email">Email Address:</label>
+            <input
+              type="text"
+              {...register("email")}
+              placeholder="Email Address"
+              className={`bg-gray-700 text-gray-100 rounded-md border  p-2 outline-none ${
+                errors.email ? "border-red-500" : "border-white/20"
+              }`}
+            />
+            <span className="text-red-500 text-sm">
+              {errors.email?.message}
+            </span>
+          </div>
+          <div className="flex flex-col gap-2">
+            <label htmlFor="password">Password:</label>
+            <input
+              type="password"
+              {...register("password")}
+              placeholder="Password"
+              className={`bg-gray-700 text-gray-100 rounded-md border p-2 outline-none ${
+                errors.password ? "border-red-500" : "border-white/20"
+              }`}
+            />
+            <span className="text-red-500 text-sm ">
+              {errors.password?.message}
+            </span>
+          </div>
+          <div className="flex flex-col gap-2">
+            <label htmlFor="confirmPassword">Confirm Password:</label>
+            <input
+              type="password"
+              {...register("confirmPassword")}
+              placeholder="Confirm Password"
+              className={`bg-gray-700 text-gray-100 rounded-md border p-2 outline-none ${
+                errors.confirmPassword ? "border-red-500" : "border-white/20"
+              }`}
+            />
+            <span className="text-red-500 text-sm ">
+              {errors.confirmPassword?.message}
+            </span>
+          </div>
           <button className="bg-blue-500 text-white rounded-md p-2 mt-3 cursor-pointer hover:bg-blue-600 transition">
             Register
           </button>
