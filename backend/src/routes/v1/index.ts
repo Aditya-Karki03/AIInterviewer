5c07c4e1aeaff85ec26c0ae698e57472e4f67af8
Aditya Karki <adityakarki201@gmail.com>
2025-09-03 21:38:41 +0530

    Add: Dashboard Routing

--- backend/src/routes/v1/index.ts
+++ backend/src/routes/v1/index.ts
@@ -2,6 +2,7 @@ import express from "express";
 import { userAuth } from "../../middleware/userAuth";
 import { authRouter } from "./authRoutes";
 import { profileRouter } from "./profileRoutes";
+import { dashboardRoutes } from "./dashboardRoutes";
 
 const appRouterV1 = express.Router();
 
@@ -11,5 +12,8 @@ appRouterV1.use("/api/v1/user", authRouter);
 // user profile route
 // userAuth middleware to ensure only authenticated users can access this route
 appRouterV1.use("/api/v1/profile", userAuth, profileRouter);
+
+//dashboard route
+appRouterV1.use("/api/v1/dashboard", userAuth, dashboardRoutes);
 
 export default appRouterV1;
